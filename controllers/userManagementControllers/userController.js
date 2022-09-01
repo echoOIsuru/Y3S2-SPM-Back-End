3aaf2ab30db26262ba923c6c664fea6ca5569bdb
Isuru Sahan <90211418+IT20133290@users.noreply.github.com>
2022-09-01 22:08:07 +0530

    user management functions added

---
+++ controllers/userManagementControllers/userController.js
@@ -0,0 +1,83 @@
+const userModel = require("../../models/userManagement/UserModel_UM");
+
+
+exports.addUser = async (req, res) => {
+    if (!req.body) {
+        res.status(400).send({ message: "Content can not be empty!" })
+        return;
+    }
+
+    const record = new userModel({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        address: req.body.address,
+        city: req.body.city,
+        password: req.body.password,
+        userType: req.body.userType,
+        specialization: req.body.specialization,
+        date: new Date().toString()
+    })
+
+    record
+        .save(record)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some erro occurred while creating"
+            })
+        })
+
+}
+
+exports.getAllUsers = (req, res) => {
+    try {
+        userModel.find({}, (err, result) => {
+            if (err)
+                console.log(err);
+
+            res.send(result)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.updateUser = (req, res) => {
+    try {
+        userModel.findByIdAndUpdate(req.params.id, req.body, async (err, result) => {
+            if (err)
+                console.log(err)
+            await res.send(result)
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.deleteUser = (req, res) => {
+    try {
+        userModel.findByIdAndDelete(req.params.id, (err, result) => {
+            if (err)
+                console.log(err)
+            res.send(result)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.login = (req, res) => {
+    try {
+        userModel.findOne(req.body, (err, result) => {
+            if (err)
+                console.log(err);
+            res.send(result)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,15 @@ const express = require('express');
 var router = express.Router();
 
 const test = require('../controllers/userManagementControllers/test')
+const userController = require('../controllers/userManagementControllers/userController')
 
 router.post('/', test.test)
+
+//User Management Routers
+router.post('/user_um', userController.addUser);
+router.get('/all_users_um', userController.getAllUsers);
+router.put('/user_um/:id', userController.updateUser);
+router.delete('/user_um/:id', userController.deleteUser);
+router.post('/user_validate', userController.login);
 
 module.exports = router
