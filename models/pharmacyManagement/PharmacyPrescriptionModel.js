6c94606b0c4c499a8cfa3083ddce14395d16e755
Sandali Randima <sandali@Sandalis-MacBook-Pro.local>
2022-09-01 22:03:08 +0530

    added crud methods

---
+++ models/pharmacyManagement/PharmacyPrescriptionModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const PharmacyPrescriptionSchema = new Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
+    total_bill: {
+        type: String,
+        required: true
+    },
+    added_date: {
+        type: String,
+        required: true
+    },
+    medicines: {
+        type: Array,
+        required: true
+    }
+});
+
+const PharmacyPrescriptionModel = mongoose.model('Pharmacy_PrescriptionModel', PharmacyPrescriptionSchema);
+module.exports = PharmacyPrescriptionModel;
--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,16 @@ const express = require('express');
 var router = express.Router();
 
 const test = require('../controllers/userManagementControllers/test')
+const PharmacyController = require('../controllers/pharmacyManagementControllers/PharmacyController')
 
 router.post('/', test.test)
+
+/**
+ * Pharmacy management routes
+ */
+router.post('/pm_add_stock', PharmacyController.addStock);
+router.get('/pm_get_stocks', PharmacyController.retrieveStocks);
+router.put('/pm_update_stock/:id', PharmacyController.updateStock);
+router.delete('/pm_delete_stock/:id', PharmacyController.deleteStock);
 
 module.exports = router
