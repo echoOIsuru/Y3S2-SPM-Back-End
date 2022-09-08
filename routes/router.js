3fb725180bcdee6a746303814ab3b6495f10bd38
yasiruranathunga <yasirusr99@gmail.com>
2022-09-08 20:19:44 +0530

    8

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const test = require('../controllers/userManagementControllers/test')
 const appointment=require('../controllers/patient/appointment')
-
+const d=require('../controllers/patient/d')
 
 router.post('/appointment/', appointment.appointment)
 router.get('/appointment/:id', appointment.find)
@@ -11,4 +11,9 @@ router.get('/appointment/byid/:id', appointment.findbyid)
 router.get('/appointment', appointment.findall)
 router.delete('/appointment/:id',  appointment.delete);
 router.put('/appointment/update:id',  appointment.update);
+
+
+router.post('/d/', d.d)
+router.get('/d/', d.findall)
+
 module.exports = router
