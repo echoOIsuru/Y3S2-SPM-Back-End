61445178a060a590445d2e6f047f5c013dd01577
Sandali-R <it20218058@my.sliit.lk>
2022-10-13 20:01:35 +0530

    add prescription api calls created

--- routes/router.js
+++ routes/router.js
@@ -14,6 +14,9 @@ router.get('/pm_get_stocks', PharmacyController.retrieveStocks);
 router.get('/pm_get_stock/:id', PharmacyController.retrieveStock);
 router.put('/pm_update_stock/:id', PharmacyController.updateStock);
 router.delete('/pm_delete_stock/:id', PharmacyController.deleteStock);
+router.post('/pm_check_medicine', PharmacyController.checkMedicine);
+router.post('/pm_add_prescription', PharmacyController.addPrescription);
+
 
 router.post('/appointment', appointment.appointment)
 router.get('/appointment/:id', appointment.find)
